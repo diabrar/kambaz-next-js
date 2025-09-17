5623f832b13ca50d54cd8155027ae0d4640881d4
Dia Brar <diakb42@gmail.com>
2025-09-17 13:47:42 -0400

    fixed navigation links w/ placeholders

---
+++ app/(Kambaz)/Courses/[cid]/Grades/page.tsx
@@ -0,0 +1,7 @@
+export default function Grades() {
+    return (
+        <div id="wd-grades">
+            <h1>Grades placeholder</h1>
+        </div>
+    )
+}
--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -4,11 +4,11 @@ export default function CourseNavigation() {
     <div id="wd-courses-navigation">
       <Link href="/Courses/1234/Home" id="wd-course-home-link">Home</Link><br/>
       <Link href="/Courses/1234/Modules" id="wd-course-modules-link">Modules</Link><br/>
-      <Link href="/Courses/1234/Piazza" id="wd-course-piazza-link">Piazza</Link><br/>
-      <Link href="/Courses/1234/Zoom" id="wd-course-zoom-link">Zoom</Link><br/>
-      <Link href="/Courses/1234/Assignments" id="wd-course-quizzes-link">Assignments</Link><br/>
-      <Link href="/Courses/1234/Quizzes" id="wd-course-assignments-link">Quizzes</Link><br/>
+      <Link href="https://piazza.com/" id="wd-course-piazza-link">Piazza</Link><br/>
+      <Link href="https://www.zoom.com/" id="wd-course-zoom-link">Zoom</Link><br/>
+      <Link href="/Courses/1234/Assignments" id="wd-course-assignments-link">Assignments</Link><br/>
+      <Link href="/Courses/1234/Quizzes" id="wd-course-quizzes-link">Quizzes</Link><br/>
       <Link href="/Courses/1234/Grades" id="wd-course-grades-link">Grades</Link><br/>
-      <Link href="/Courses/1234/People/Table" id="wd-course-people-link">People</Link><br/>
+      <Link href="/Courses/1234/People" id="wd-course-people-link">People</Link><br/>
     </div>
   );}
